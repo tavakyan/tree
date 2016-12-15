6f04acb067f5a41431eec756cb5ece5af4f5779b
tetriseffect <avakyan@gmail.com>
2016-12-14 19:17:16 -0800

    Added tests for left and right child node

--- tree.go
+++ tree.go
@@ -8,8 +8,18 @@ type INode interface {
 	GetElement() IElement
 }
 
+type IBNode interface {
+	GetElement() IElement
+	GetLeft() IBNode
+	GetRight() IBNode
+}
+
 type ITree interface {
 	GetRoot() INode
+}
+
+type IBTree interface {
+	GetRoot() IBNode
 }
 
 type AVLElement struct {
@@ -17,16 +27,26 @@ type AVLElement struct {
 
 type AVLNode struct {
 	element AVLElement
+	left *AVLNode
+	right *AVLNode
 }
 
 func (n AVLNode) GetElement() IElement {
 	return n.element
 }
 
+func (n AVLNode) GetLeft() IBNode {
+	return n.left
+}
+
+func (n AVLNode) GetRight() IBNode {
+	return n.right
+}
+
 type AVLTree struct {
 	root AVLNode
 }
 
-func (t AVLTree) GetRoot() INode {
+func (t AVLTree) GetRoot() IBNode {
 	return t.root
 }
--- tree_test.go
+++ tree_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAVLTreeGetRootIsAVLNode(t *testing.T) {
-	var tree ITree = new(AVLTree)
+	var tree IBTree = new(AVLTree)
 	r := tree.GetRoot()
 	n, ok := r.(AVLNode)
 	if !ok {
@@ -15,12 +15,32 @@ func TestAVLTreeGetRootIsAVLNode(t *testing.T) {
 	}
 }
 
-// func TestAVLNodeGetElementIsAVLElement(t *testing.T) {
-// 	var node INode = new(AVLNode)
-// 	e := node.GetElement()
-// 	e1, ok := e.(AVLElement)
-// 	if !ok {
-// 		fmt.Println(e1)
-// 		t.Error()
-// 	}
-// }
+func TestAVLNodeGetElementIsAVLElement(t *testing.T) {
+	var n INode = new(AVLNode)
+	e := n.GetElement()
+	e1, ok := e.(AVLElement)
+	if !ok {
+		fmt.Println(e1)
+		t.Error()
+	}
+}
+
+func TestAVLNodeGetLeftIsAVLNode(t *testing.T) {
+	var n IBNode = new(AVLNode)
+	l := n.GetLeft()
+	l1, ok := l.(*AVLNode)
+	if !ok {
+		fmt.Println(l1)
+		t.Error()
+	}
+}
+
+func TestAVLNodeGetRightIsAVLNode(t *testing.T) {
+	var n IBNode = new(AVLNode)
+	r := n.GetRight()
+	r1, ok := r.(*AVLNode)
+	if !ok {
+		fmt.Println(r1)
+		t.Error()
+	}
+}
